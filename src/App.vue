fb2f0cb8d41a813633b9c40ff4b8482db1e812b1
stonesaw <mail.sou.dev@gmail.com>
2022-01-29 05:18:39 +0900

    add: cli base

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Terminal />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Terminal from './components/Terminal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Terminal
   }
 }
 </script>
 
 <style>
+:root {
+  --font-main: rgba(255, 255, 255, .95);
+  --bg-main: #222;
+}
+
+html, body {
+  padding: 0;
+  margin: 0;
+  background-color: var(--bg-main);
+}
+
+button, input, optgroup, select, textarea {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  vertical-align: middle;
+  color: inherit;
+  font: inherit;
+  border: 0;
+  background: rgba(0, 0, 0, 0);
+  padding: 0;
+  margin: 0;
+  outline: 0;
+  border-radius: 0;
+  text-align: inherit;
+}
+
+::-moz-selection { /* Code for Firefox */
+  color: var(--bg-main);
+  background: var(--font-main);
+}
+
+::selection {
+  color: var(--bg-main);
+  background: var(--font-main);
+}
+
+a {
+  color: #4293b9;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--font-main);
 }
 </style>
---
+++ src/components/Terminal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="terminal">
+    <div>Hello, World!</div>
+    <div class="cli-aa">
+      <div>&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;________&nbsp;&nbsp;&nbsp;&nbsp;____&nbsp;&nbsp;&nbsp;__</div>
+      <div>|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;/__&nbsp;&nbsp;/&nbsp;/________&nbsp;&nbsp;____&nbsp;___&nbsp;&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;/_____&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;____/&nbsp;/&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;_/&nbsp;&nbsp;/&nbsp;/</div>
+      <div>|&nbsp;|&nbsp;/|&nbsp;/&nbsp;/&nbsp;_&nbsp;\/&nbsp;/&nbsp;___/&nbsp;__&nbsp;\/&nbsp;__&nbsp;`__&nbsp;\/&nbsp;_&nbsp;\&nbsp;&nbsp;&nbsp;/&nbsp;__/&nbsp;__&nbsp;\&nbsp;&nbsp;&nbsp;/&nbsp;/&nbsp;&nbsp;&nbsp;/&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;/&nbsp;&nbsp;&nbsp;/&nbsp;/&nbsp;</div>
+      <div>|&nbsp;|/&nbsp;|/&nbsp;/&nbsp;&nbsp;__/&nbsp;/&nbsp;/__/&nbsp;/_/&nbsp;/&nbsp;/&nbsp;/&nbsp;/&nbsp;/&nbsp;/&nbsp;&nbsp;__/&nbsp;&nbsp;/&nbsp;/_/&nbsp;/_/&nbsp;/&nbsp;&nbsp;/&nbsp;/___/&nbsp;/____/&nbsp;/&nbsp;&nbsp;&nbsp;/_/&nbsp;&nbsp;</div>
+      <div>|__/|__/\___/_/\___/\____/_/&nbsp;/_/&nbsp;/_/\___/&nbsp;&nbsp;&nbsp;\__/\____/&nbsp;&nbsp;&nbsp;\____/_____/___/&nbsp;&nbsp;(_)&nbsp;&nbsp;&nbsp;</div>
+    </div>
+    <div>Welcome to cli</div>
+    <div class="line">
+      <span class="cli-head">stonesaw.github.io $ </span>
+      <span ref="currentInput" class="cli-input">
+        <input type="text" v-model="inputText">
+
+      </span>
+    </div>
+    <!--  -->
+    <div class="line">
+      <span class="cli-result">input: {{ inputText }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Terminal',
+  data() {
+    return {
+      inputText: ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+.terminal {
+  font-family: 'Roboto Mono', monospace;
+  padding: 10px;
+}
+
+.cli-head {
+  color: aquamarine;
+}
+
+.cli-head::selection {
+  color: red;
+}
+</style>
