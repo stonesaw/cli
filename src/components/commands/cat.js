c6db18822ec94655b74c31ef8b4860189cfa6412
stonesaw <mail.sou.dev@gmail.com>
2022-02-11 18:03:22 +0900

    add: cat

---
+++ src/components/commands/cat.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+
+import directory from './../../assets/directory.json'
+
+
+export default function (current_dir, fname) {
+    return `print file content (${fname}, ${current_dir})`
+}
